672050d794dcce0af8f7aa355b444b39754e7a56
Zarivate <Zarivate1@gmail.com>
2023-05-21 23:59:26 -0500

    Fixed minor mongodb glitch where data wouldn't appear

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,18 @@
 // Component that shows the live feed, if any, to the user
 
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return <div>Feed Component here</div>;
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
